1f8c5798b832381804cb4313053687bef2ef5e4d
Chanwit Saisin <chanwit.ssin@gmail.com>
2024-01-17 03:25:26 +0700

    fix grpc url

--- gateway/src/config/grpc.config.ts
+++ gateway/src/config/grpc.config.ts
@@ -3,21 +3,21 @@ import { join } from 'path';
 
 const protoDir = join(__dirname, '/', 'proto');
 
-export const grpcAuthClientConfig: ClientProviderOptions = {
-  name: 'AUTH_PACKAGE',
-  transport: Transport.GRPC,
-  options: {
-    url: '0.0.0.0:50051',
-    package: ['auth'],
-    protoPath: [join(protoDir, '/auth.proto')],
-  },
-};
+// export const grpcAuthClientConfig: ClientProviderOptions = {
+//   name: 'AUTH_PACKAGE',
+//   transport: Transport.GRPC,
+//   options: {
+//     url: '0.0.0.0:50051',
+//     package: ['auth'],
+//     protoPath: [join(protoDir, '/auth.proto')],
+//   },
+// };
 
 export const grpcUserClientConfig: ClientProviderOptions = {
   name: 'USER_PACKAGE',
   transport: Transport.GRPC,
   options: {
-    url: '0.0.0.0:50052',
+    url: process.env.USER_SERVICE_URL || '0.0.0.0:50052',
     package: ['user'],
     protoPath: [join(protoDir, '/user.proto')],
   },
